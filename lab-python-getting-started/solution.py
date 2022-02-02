90db31ef4c70f86bf18314978f31df149466acba
Dean Matthews <drm9114@nyu.edu>
2022-02-02 16:26:35 -0500

    Pushing solution.py to GitHub

--- lab-python-getting-started/solution.py
+++ lab-python-getting-started/solution.py
@@ -45,8 +45,8 @@ if __name__ == "__main__":
     debug_question = 'What is the MD5 hashing value to the following message: \'NYU Computer Networking\' - ' \
                      'Use MD5 hash generator and use the answer in your code'
     print(welcome_assignment_answers(debug_question))
-    debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+    #debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
+    #print(welcome_assignment_answers(debug_question))
     debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - " \
                      "The answer should be a numeric number"
     print(welcome_assignment_answers(debug_question))
