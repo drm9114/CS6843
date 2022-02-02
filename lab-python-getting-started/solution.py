ce15f3172cb49e6198f5c666899ee426b1f384af
Dean Matthews <drm9114@nyu.edu>
2022-02-02 17:25:56 -0500

    Pushing solution.py to GitHub

--- lab-python-getting-started/solution.py
+++ lab-python-getting-started/solution.py
@@ -53,13 +53,3 @@ if __name__ == "__main__":
     debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - " \
                      "The answer should be a numeric number"
     print(welcome_assignment_answers(debug_question))
-###Questions:
-###"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-###"Are encoding and encryption the same? - Yes/No"
-###"Is it possible to decrypt a message without a key? - Yes/No"
-###"Is it possible to decode a message without a key? - Yes/No"
-###"Is a hashed message supposed to be un-hashed? - Yes/No"
-###"What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
-###"Is MD5 a secured hashing algorithm? - Yes/No"
-###"What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
-###"What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
