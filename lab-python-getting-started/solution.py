a6419978da682a0da01b796c1e517ba11923e5a9
Dean Matthews <drm9114@nyu.edu>
2022-02-03 09:16:55 -0500

    Pushing solution.py to GitHub

--- lab-python-getting-started/solution.py
+++ lab-python-getting-started/solution.py
@@ -47,9 +47,9 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question))
     debug_question = "Is MD5 a secured hashing algorithm? - Yes/No"
     print(welcome_assignment_answers(debug_question))
-    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - " \
-                     "The answer should be a numeric number"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - " \
-                     "The answer should be a numeric number"
-    print(welcome_assignment_answers(debug_question))
+   # debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - " \
+   #                  "The answer should be a numeric number"
+   # print(welcome_assignment_answers(debug_question))
+   # debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - " \
+   #                  "The answer should be a numeric number"
+   # print(welcome_assignment_answers(debug_question))
